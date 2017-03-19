6db37aa7e9e35898a4416476d4a5672e29941db5
Aaron Butler <Aaron Butler>
2017-03-19 13:52:13 -0700

    Working cardboard version with reactive gaze retical.

---
+++ Assets/Scripts/Instantiation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiation : MonoBehaviour
+{
+
+    public Transform Prefab;
+
+    void Start()
+    {
+        Instantiate(Prefab, new Vector3(0, 3, 10), Quaternion.Euler(0, 0, 0));
+    }
+}
